a1c7b840e9604d08b03c1ab6101f9ee9ae0d3e95
SakinahAlnajjar <sooka.1419@gmail.com>
2023-08-03 00:46:10 +0300

    Initial Commit

---
+++ Week1-Day2-Lab.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+var score: Int = 2
+
+score += 10
+print(score)
+
+if score > 9 {
+    print("Pass")
+} else {
+    print("Fail")
+}
+
+var arr = ["Sakinah", "Fatima","sara","Hoor","Dalia"]
+
+for i in arr {
+   print(i)
+}
+
+var fruit = ["apple":3, "orange":2, "banana":4,"kiwi":5, "peach":2]
+
+for i in fruit {
+    print(i)
+}
+
+let name = "Sakinah"
+print("Welcome \(name)")
+
+
